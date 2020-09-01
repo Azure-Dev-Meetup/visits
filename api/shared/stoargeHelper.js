2ea5245e30803802a617f21077677476d156e44b
Azuredevmuc <65605342+Azuredevmuc@users.noreply.github.com>
2020-09-02 01:06:59 +0200

    Update stoargeHelper.js
    removed unused const

--- api/shared/stoargeHelper.js
+++ api/shared/stoargeHelper.js
@@ -2,26 +2,10 @@ const { BlobServiceClient, ContainerClient } = require('@azure/storage-blob');
 const uuid = require('uuid');
 const blobSasUrl = "https://visitsstrore.blob.core.windows.net/visitsdata?sp=racwdl&st=2020-09-01T22:23:40Z&se=2020-09-05T22:23:00Z&sv=2019-12-12&sr=c&sig=FFjJV%2BYRuxFolq%2B7H2VJNmF3I9%2FQiKRjEig3zgan9NM%3D";
 
-const _visit = {
-    id: undefined,
-    numberOfVisitors: "",
-    name: "",
-    date: "",
-    arrivalTime: "",
-    leavingTime: "",
-    tableNumber: "",
-    phone: "",
-    street: "",
-    plz: "",
-    city: "",
-    email: "",
-};
 
 module.exports = async function (visit) {
 
     const containerClient = new ContainerClient(blobSasUrl);
-
-
     //Set the visit ID to UUID
     visit.id = uuid.v4();
     // Create a unique name for the blob
